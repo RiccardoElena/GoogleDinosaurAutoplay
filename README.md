d3d8fe6fc72241407d3957ce55938ab9d88a7dec
Riccardo Elena <witherheart63@gmail.com>
2020-09-11 19:45:26 +0200

    Google Dinosaur Cheat

---
+++ README.md
@@ -0,0 +1,5 @@
+**DON'T RUN THE FILE TO USE THE CHAET**
+
+1. Open Chrome and search chrome://dino (or turn off connection)
+2. Open the Console with CTRL+SHIFT+i (or going through Inspector)
+3. Copy the code in the Console and press Enter
---
+++ cheat.js
@@ -0,0 +1,22 @@
+const origin = Runner.prototype.gameOver
+
+Runner.prototype.gameOver = () => {}
+
+const autoPlayLoop = () => {
+  const JUMP_SPEED = 50
+  const DISTANCE_BEFORE_JUMP = 120
+  const instance = window.Runner.instance_
+  const tRex = instance.tRex
+  if (tRex.jumping) {
+    requestAnimationFrame(autoPlayLoop)
+    return
+  }
+  const tRexPos = tRex.xPos
+  const obstacles = instance.horizon.obstacles
+  const nextObstacle = obstacles.find(o => o.xPos > tRexPos)
+  if (nextObstacle && (nextObstacle.xPos - tRexPos) <= DISTANCE_BEFORE_JUMP) {
+    tRex.startJump(JUMP_SPEED)
+  }
+  requestAnimationFrame(autoPlayLoop);
+}
+requestAnimationFrame(autoPlayLoop)
